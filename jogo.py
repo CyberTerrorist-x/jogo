abe68944bd2a8ac12fe5f5f69f14cfab4ea1ee32
CyberTerrorist-x <84683727+CyberTerrorist-x@users.noreply.github.com>
2021-09-03 19:54:48 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,4 +1,23 @@
 from random import randint
+import time
+import os
+
+#cores 
+red = "\033[1;91m"
+blu = "\033[1;94m"
+gre = "\033[1;92m"
+yel = "\033[1;33m"
+mag = "\033[1;95m"
+
+def clear():os.system('clear')
+
+print(f"{yel}Cyber Gay kkkkkkkkkkk")
+time.sleep(0.6)
+clear()
+
+nm = input(f'{gre}Opaa Qual o seu nick???:  ')
+print(f"{yel}Iai {nm}")
+print(" ")
 
 n = int(randint(0, 10))
 
@@ -8,20 +27,23 @@ t = 0
 
 while p != n:
 
-    p = int(input('Digite um numero: '))
+    p = int(input(f'{red}Digite um numero {nm}: '))
 
     t+= 1
 
     if p == n:
 
-        print('Acertou placar {}'.format(t))
+        print(f'{blu}Acertou placar {t}')
 
     elif p <n:
 
-        print('Digite um numero maior: ')
+        print(f'{mag}Digite um numero maior: ')
 
     else:
 
-        print('Digite um numero menor: ')
+        print(f'{gre}Um numero Menor!')
+        print(f'{blu}Game Over :( ')
+        print(" ")
 
-        print('Fim do jogo!')
+
+        
